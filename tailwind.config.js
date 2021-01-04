4fda1f51c29c362d8e8a61832d9385bb8a65bd93
Ramil <ramazu@github.com>
2021-01-04 13:40:18 -0800

    Test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
 // tailwind.config.js
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
-      },
+    fontFamily: {
+      custom: ['Inter', 'sans-serif'],
     },
   },
   variants: {
